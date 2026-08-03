72de0c6f1b787021f0f5b37d7f1d3bc6d8cd92e0
ayanb5458-dot <ayanb5458@gmail.com>
2026-08-03 15:17:04 +0500

    Add files via upload

---
+++ ayan.py
@@ -0,0 +1,8 @@
+first_name="tony"
+last_name="stark"
+age="53"
+height="1.85m"
+print("first_name=",first_name)
+print("last_name=",last_name)
+print("tony age is =", age)
+print(" tony height is =", height)
